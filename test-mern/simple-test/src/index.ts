0b28df023e1c3eba30b5ba9c956b6495737dd4d9
Bhanu Rathore <120242274+bhanuo07@users.noreply.github.com>
2024-08-29 02:15:02 +0530

    test-mern:Worked on mockign the db calls

--- test-mern/simple-test/src/index.ts
+++ test-mern/simple-test/src/index.ts
@@ -1,13 +1,21 @@
 import express from "express";
+import { db } from "./db";
 const app = express();
 app.use(express.json());
-app.post("/sum", (req, res) => {
-  const { a, b } = req.body;
-
-  let ans = a + b;
-  res.json({
+app.post("/sum", async (req, res) => {
+  const { a, b } = req.body as { a: number; b: number };
+  let result = a + b;
+  let data = await db.calculation.create({
+    data: {
+      a,
+      b,
+      result,
+    },
+  });
+  return res.json({
     success: true,
-    ans,
+    a: true,
+    result,
   });
 });
 
--- test-mern/simple-test/src/tests/index.test.ts
+++ test-mern/simple-test/src/tests/index.test.ts
@@ -1,15 +1,15 @@
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect, vi } from "vitest";
 import request from "supertest";
+vi.mock("../db");
 import app from "..";
-
 describe("POST /sum", () => {
-  it("should return 2 + 1 equals 3", async () => {
-    const response = await request(app).post("/sum").send({
-      a: 1,
+  it("Should return the result 2+2", async () => {
+    const res = await request(app).post("/sum").send({
+      a: 2,
       b: 2,
     });
-    // console.log(response.body);
-    expect(response.statusCode).toBe(200);
-    expect(response.body.ans).toBe(3);
+    // console.log(res.body);
+    expect(res.statusCode).toBe(200);
+    expect(res.body.result).toBe(4);
   });
 });
